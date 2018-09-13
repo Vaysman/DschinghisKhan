6137fd1cc7ab5ec31db81b5ba4b0955b2dfc3214
rKurbanov <spiritblossom@icloud.com>
2018-09-13 05:36:19 +0300

    #27 register email is now not sent if a company with specified INN already exists

--- src/main/java/ru/dao/entity/listener/CompanyListener.java
+++ src/main/java/ru/dao/entity/listener/CompanyListener.java
@@ -12,6 +12,7 @@ import ru.dao.entity.Company;
 import ru.dao.entity.Contact;
 import ru.dao.entity.Point;
 import ru.dao.entity.User;
+import ru.dao.repository.CompanyRepository;
 import ru.dao.repository.ContactRepository;
 import ru.dao.repository.PointRepository;
 import ru.dao.repository.UserRepository;
@@ -21,8 +22,6 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import javax.persistence.PostPersist;
 import javax.persistence.PrePersist;
-import java.util.HashSet;
-import java.util.Set;
 
 import static ru.util.Translit.cyr2lat;
 import static ru.util.Translit.removeAbbreviations;
@@ -35,24 +34,32 @@ public class CompanyListener {
     private static UserRepository userRepository;
     private static ContactRepository contactRepository;
     private static PointRepository pointRepository;
+    private static CompanyRepository companyRepository;
 
     @Autowired
-    public void init(JavaMailSender sender, UserRepository userRepository, ContactRepository contactRepository, PointRepository pointRepository){
+    public void init(JavaMailSender sender, UserRepository userRepository, ContactRepository contactRepository, PointRepository pointRepository, CompanyRepository companyRepository) {
         CompanyListener.sender = sender;
         CompanyListener.userRepository = userRepository;
         CompanyListener.contactRepository = contactRepository;
         CompanyListener.pointRepository = pointRepository;
+        CompanyListener.companyRepository = companyRepository;
     }
 
     @PrePersist
     private void prePersist(Company company) throws MessagingException {
-        if(!company.getType().equals(CompanyType.TRANSPORT)) {
-            return;
-        }else {
-            company.setShortName(removeAbbreviations(company.getName()));
-            String companyUserLogin = cyr2lat(company.getShortName().replaceAll(" ",""));
-            while (userRepository.findByLogin(companyUserLogin).isPresent()){
-                companyUserLogin= companyUserLogin+"1";
+
+
+        if (!companyRepository.findFirstByInn(company.getInn()).isPresent()) {
+
+            if (!company.getType().equals(CompanyType.TRANSPORT)) {
+                return;
+            } else {
+                company.setShortName(removeAbbreviations(company.getName()));
+
+            }
+            String companyUserLogin = cyr2lat(company.getShortName().replaceAll(" ", ""));
+            while (userRepository.findByLogin(companyUserLogin).isPresent()) {
+                companyUserLogin = companyUserLogin + "1";
             }
             String userPassword = RandomStringGenerator.randomAlphaNumeric(8);
             User user = User.builder()
@@ -63,9 +70,9 @@ public class CompanyListener {
                     .email(company.getEmail())
                     .passAndSalt(userPassword)
                     .build();
-            Set<User> userList = new HashSet<>();
-            userList.add(user);
-            company.setUsers(userList);
+//        Set<User> userList = new HashSet<>();
+//        userList.add(user);
+//        company.setUsers(userList);
 
             Point point = new Point();
             pointRepository.save(point);
@@ -77,24 +84,24 @@ public class CompanyListener {
             helper.setFrom("tarificationsquad@gmail.com");
             helper.setTo(company.getEmail());
             helper.setText("Зарегистрирован пользователь для транспортой компании: " +
-                    company.getName()+
-                    "\nЛогин: "+user.getLogin()+
-                    "\nПароль: "+userPassword);
+                    company.getName() +
+                    "\nЛогин: " + user.getLogin() +
+                    "\nПароль: " + userPassword);
             helper.setSubject("Регистрационные данные");
-            if(!company.getEmail().equals("test@tesе.test")) sender.send(message);
-        }
+            if (!company.getEmail().equals("test@tesе.test")) sender.send(message);
 
+            Contact contact = Contact.builder()
+                    .company(company)
+                    .email(company.getEmail())
+                    .type(ContactType.PRIMARY)
+                    .build();
+
+            contactRepository.save(contact);
+        }
 
     }
 
     @PostPersist
-    public void postPersist(Company company){
-        Contact contact = Contact.builder()
-                .company(company)
-                .email(company.getEmail())
-                .type(ContactType.PRIMARY)
-                .build();
-
-        contactRepository.save(contact);
+    public void postPersist(Company company) throws MessagingException {
     }
 }
